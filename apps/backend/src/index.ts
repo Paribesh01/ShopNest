c33e92e2ed7e0e60ebfbcc232494ef2b9e192a47
Paribesh01 <nepalparibesh01@gmail.com>
2025-02-12 20:30:18 +0530

    init

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
