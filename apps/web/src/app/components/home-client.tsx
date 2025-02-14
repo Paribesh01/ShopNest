32b77ba84681adfa925bb32f13f04fcede93d8f2
Paribesh01 <nepalparibesh01@gmail.com>
2025-02-14 15:04:23 +0530

    fix: minor changes

---
+++ apps/web/src/app/components/home-client.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Button } from "@repo/ui/button";
+import { Input } from "@repo/ui/input";
+
+export function HomeClient() {
+  return (
+    <>
+      this is the landing page
+      <Button>this is the button</Button>
+      <Input />
+    </>
+  );
+}
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@repo/ui/button";
 export default function Home() {
-  return <>this is the landing page</>;
+  return (
+    <>
+      <div className="text-red-500">button</div>
+
+      <Button variant="destructive">Delete</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="ghost">Ghost</Button>
+    </>
+  );
 }
