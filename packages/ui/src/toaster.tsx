cf8b1b00e270f05ea05094b7c167c3bb42df5cd4
Paribesh01 <nepalparibesh01@gmail.com>
2025-02-15 17:50:12 +0530

    fix: toast issue

--- packages/ui/src/toaster.tsx
+++ packages/ui/src/toaster.tsx
@@ -1,4 +1,5 @@
-import { useToast } from "@/hooks/use-toast"
+"use client";
+import { useToast } from "../hooks/use-toast";
 import {
   Toast,
   ToastClose,
@@ -6,10 +7,10 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/src/toast"
+} from "../src/toast";
 
 export function Toaster() {
-  const { toasts } = useToast()
+  const { toasts } = useToast();
 
   return (
     <ToastProvider>
@@ -25,9 +26,9 @@ export function Toaster() {
             {action}
             <ToastClose />
           </Toast>
-        )
+        );
       })}
       <ToastViewport />
     </ToastProvider>
-  )
+  );
 }
